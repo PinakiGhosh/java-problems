7356694e62f089e3bb59969710bc014eb22f3c43
Obed Jamir <obedjamir123@gmail.com>
2022-04-08 14:44:53 +0530

    First Spring Boot Project Used lombok library

--- src/main/java/com/bankbazaar/test/model/TestModel1.java
+++ src/main/java/com/bankbazaar/test/model/TestModel1.java
@@ -1,14 +1,9 @@
 package com.bankbazaar.test.model;
 
+import lombok.Data;
+
+@Data
 public class TestModel1 {
 
     private String data ="Data1";
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
 }
--- src/main/java/com/bankbazaar/test/model/TestModel2.java
+++ src/main/java/com/bankbazaar/test/model/TestModel2.java
@@ -1,17 +1,10 @@
 package com.bankbazaar.test.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class TestModel2 {
     private String data;
-
-    public TestModel2(String data) {
-        this.data = data;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
 }
